445c0cd430872319c0558c5ecb6b40daae8c9984
Yunus O. KAREEM <solayunus19@gmail.com>
2022-02-13 12:16:55 +0000

    first commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const employeeRoutes = require('./routes/employees.routes')
+
+const app = express();
+ 
+app.use(cors())
+
+// setup the server port
+const port = process.env.PORT || 5000;
+
+// parse request data content type application/x-www-form-rulencoded
+app.use(bodyParser.urlencoded({extended: false}));
+ 
+// parse request data content type application/json
+app.use(bodyParser.json());
+
+// define root route
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+});
+
+// create employee routes
+app.use('/api/v1/employee', employeeRoutes);
+ 
+// listen to the port
+app.listen(port, ()=>{
+    console.log(`Express is running at port ${port}`);
+});
