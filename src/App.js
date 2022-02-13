173c7d1cf4da9fa63c031d5970d71bf5aa3954e6
Yunus O. KAREEM <solayunus19@gmail.com>
2022-02-13 12:07:41 +0000

    Mern_Crude update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import EmployeeDetail from './pages/EmployeeDetails';
+import EditEmployee from './pages/EditEmployee';
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
 
-function App() {
+
+function App(props) {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<EmployeeDetail/>} />
+        <Route exact path="/EditEmployee/editID/:id" element={<EditEmployee/>} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
